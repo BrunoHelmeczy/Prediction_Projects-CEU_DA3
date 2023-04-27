0cd76e7c44a9e42fe65b6ab47e7496d5fc0c3330
Bruno Helmeczy <helmeczybruno32@gmail.com>
2023-04-27 09:55:49 +0200

    add geo + prop info cleaning

--- Bangkok_Airbnb/Code/Python/airbnb_code_cleaning.py
+++ Bangkok_Airbnb/Code/Python/airbnb_code_cleaning.py
@@ -1,4 +1,4 @@
-# import pandas as pd
+import pandas as pd
 import numpy as np
 from Bangkok_Airbnb.Code.Python.utils import *
 
@@ -14,8 +14,8 @@ VarDescription = DescribeVariables(Bangkok)
 # 2.1 Drop ID cols
 IDcols = VarDescription[VarDescription['VarGroups'] == 'IDs'].index
 
-Bangkok.drop(IDcols, inplace = True, axis = 1, errors = 'ignore')
-VarDescription.drop(IDcols, axis = 0, inplace = True, errors = 'ignore')
+DropCols(Bangkok, IDcols)
+DropRows(VarDescription, IDcols)
 
 # 2.2 logicals --> convert to ints
 LogicCols = ['host_is_superhost', 'host_has_profile_pic', 'host_identity_verified', 'has_availability', 'instant_bookable']
@@ -25,7 +25,7 @@ convertBooleans(
     Cols = LogicCols
 )
 
-VarDescription.drop(LogicCols, axis = 0, inplace = True, errors = 'ignore')
+DropRows(VarDescription, LogicCols)
 
 # 2.3 Host info
 HostCols = VarDescription[VarDescription['VarGroups'] == 'Host'].index
@@ -36,13 +36,14 @@ PercCols = ["host_response_rate","host_acceptance_rate"]
 Bangkok.loc[:, PercCols] = Bangkok.loc[:, PercCols].replace(r"%", '', regex = True).fillna(0)
 Bangkok.rename({col: f"p_{col}" for col in PercCols}, inplace = True)
 
-VarDescription.drop(PercCols, axis = 0, inplace = True, errors = 'ignore')
+DropRows(VarDescription, PercCols)
 
 HostCols = HostCols[np.logical_not(HostCols.isin(PercCols))]
 
 # 2.3.2 Verifications --> Nr host verifications
 Bangkok['n_host_verifications'] = Bangkok['host_verifications'].str.split(',').apply(len)
-Bangkok.drop('host_verifications', axis = 1, inplace = True)
+
+DropCols(Bangkok, 'host_verifications')
 
 # 2.3.3 neighbourhood --> top3 + 'Other'
 Bangkok['host_neighbourhood'].replace(r'[0-9]|Lower|Upper', '', regex = True, inplace = True)
@@ -57,8 +58,6 @@ HostCols = HostCols[np.logical_not(HostCols.isin(['host_verifications', 'host_ne
 
 # 2.3.4 listing cols
 ListingCols = HostCols[HostCols.str.contains('listing')]
-
-
 OtherListings = VarDescription[VarDescription['VarGroups'] == 'CalcListings'].index
 
 CalcColumnSimilarity(
@@ -66,8 +65,9 @@ CalcColumnSimilarity(
     ColumnList = ListingCols
 )
 
-Bangkok.drop('host_total_listings_count', axis = 1, inplace = True, errors = 'ignore')
-VarDescription.drop(ListingCols[1], axis = 0, inplace = True, errors = 'ignore')
+DropCols(Bangkok, ListingCols[1])
+DropRows(VarDescription, ListingCols[1])
+
 Bangkok['host_listings_count'].fillna(0, inplace = True)
 Bangkok['host_listings_count'] = Bangkok['host_listings_count'].apply(int)
 
@@ -78,11 +78,67 @@ CalcColumnSimilarity(
     ColumnList = AllListings
 )
 
-Bangkok.drop(AllListings[:-1], axis = 1, inplace = True, errors = 'ignore')
+DropCols(Bangkok, AllListings[:-1])
+DropRows(VarDescription, AllListings[:-1])
+
 Bangkok.rename({AllListings[-1]: f"n_{AllListings[-1]}"}, inplace = True)
 
 # 2.4 Geospatial Data
+GeoCols = VarDescription[VarDescription['VarGroups'] == 'Geo'].index
+NeighbCol = 'neighbourhood_cleansed'
+
+DropCols(Bangkok, GeoCols[GeoCols != NeighbCol])
+
+Bangkok.rename({NeighbCol: f"f_{NeighbCol}"}, inplace = True)
 # 2.5 Prop Info
+## 2.5.1 Room Types
+PropCols = VarDescription[VarDescription['VarGroups'] == 'Property'].index.to_list()
+
+DropRows(
+    Bangkok,
+    Bangkok[Bangkok['room_type'] != "Entire home/apt"].index
+)
+
+DropCols(Bangkok, 'room_type')
+PropCols.remove('room_type')
+
+## 2.5.2 Property Types
+WierdPropTypeKeys = ["Room","Castle","Entire cabin","chalet","dorm","hostel",
+    "place","Farm stay","Tiny house","Treehouse","Pension",
+    "cottage","Dome house","Earth house"]
+
+DropRows(
+    Bangkok,
+    Bangkok[Bangkok['property_type'].isin(WierdPropTypeKeys)].index
+)
+
+Bangkok['property_type'] = Bangkok['property_type'].str.replace(
+        r'serviced |Entire', '', regex = True
+    ).str.replace(
+        r'home/apt', 'apartment', regex = True
+    ).str.strip()
+
+Bangkok.loc[
+    Bangkok[np.logical_not(Bangkok['property_type'].isin(['apartment', 'condominium']))].index, 
+    'property_type'
+] = 'Other'
+
+Bangkok.rename({'property_type': 'f_property_type'}, inplace = True)
+PropCols.remove('property_type')
+
+## 2.5.3 Bathroom Text
+Bangkok['n_bathrooms'] = Bangkok['bathrooms_text'].str.replace(
+        r"Half-", '0.5' , regex = True
+    ).str.replace(
+        r"bath|s", '', regex = True
+    ).str.strip().astype('float')
+
+DropCols(Bangkok, 'bathrooms_text')
+PropCols.remove('bathrooms_text')
+
+## 2.5.4 Beds/ Bedrooms / Accommodates
+
+
 # 2.6 Sales
 # 2.7 Availability
 # 2.8 Satisfaction
